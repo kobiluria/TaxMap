8e9d02167bdbf57eb013724ff7398b0d64b2eab1
Kobi Luria <kobi@luria.net>
2014-03-26 15:36:20 -0400

    fixed loading problem.

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ L.Icon.Default.imagePath = 'node_modules/leaflet/dist/images/';
 
 cmAttr = 'Map data &copy; 2011 OpenStreetMap contributors, Imagery &copy; 2011 CloudMade';
 var OpenMapSurfer_Roads = L.tileLayer('http://129.206.74.245:8001/tms_r.ashx?x={x}&y={y}&z={z}');
-var baseLayers = ['Stamen.Watercolor', 'OpenStreetMap.Mapnik'],
+var baseLayers = [ 'OpenStreetMap.Mapnik','Stamen.Watercolor'],
     overlays = ['OpenWeatherMap.Clouds'];
 
 var leaf_map = L.map('map', {
@@ -22,4 +22,84 @@ var leaf_map = L.map('map', {
 
 var layerControl = L.control.layers.provided(baseLayers, overlays).addTo(leaf_map);
 
-var C = require('./tax_map')
+/**
+ * Created by kobi on 3/26/14.
+ */
+var geojson;
+
+var user_select = 4;
+
+var info;
+
+function getColor(d) {
+    return d > 500 ? '#BD0026' :
+        d > 200 ? '#E31A1C' :
+            d > 100 ? '#FC4E2A' :
+                d > 50 ? '#FD8D3C' :
+                    d > 20 ? '#FEB24C' :
+                        d > 10 ? '#FED976' :
+                            d > 1 ? '#800026' :
+                                '#FFEDA0';
+}
+
+function style(feature) {
+    return {
+        fillColor: getColor(feature.properties.id),
+        weight: 2,
+        opacity: 1,
+        color: 'white',
+        dashArray: '3',
+        fillOpacity: 0.7
+    };
+}
+
+function highlightFeature(e) {
+    var layer = e.target;
+
+    layer.setStyle({
+        weight: 5,
+        color: '#87AFC7',
+        dashArray: '',
+        fillOpacity: 0.99
+    });
+
+    if (!L.Browser.ie && !L.Browser.opera) {
+        layer.bringToFront();
+    }
+    info.update(layer.feature.properties);
+}
+function resetHighlight(e) {
+    geojson.resetStyle(e.target);
+    info.update();
+}
+
+function zoomToFeature(e) {
+    map.fitBounds(e.target.getBounds());
+}
+
+function filter_muni(feature,layer){
+    if (feature.properties.id == 1 ){
+        return false
+    }
+
+
+    return true;
+
+
+}
+function onEachFeature(feature, layer) {
+    if(feature.properties.id == 1){
+        return
+    }
+    layer.on({
+        mouseover: highlightFeature,
+        mouseout: resetHighlight,
+        click: zoomToFeature
+    });
+}
+
+geojson = L.geoJson(geo_data, {
+    style: style,
+    onEachFeature: onEachFeature,
+    filter: filter_muni
+}).addTo(leaf_map);
--- bundle.js
+++ bundle.js
@@ -12,7 +12,7 @@ L.Icon.Default.imagePath = 'node_modules/leaflet/dist/images/';
 
 cmAttr = 'Map data &copy; 2011 OpenStreetMap contributors, Imagery &copy; 2011 CloudMade';
 var OpenMapSurfer_Roads = L.tileLayer('http://129.206.74.245:8001/tms_r.ashx?x={x}&y={y}&z={z}');
-var baseLayers = ['Stamen.Watercolor', 'OpenStreetMap.Mapnik'],
+var baseLayers = [ 'OpenStreetMap.Mapnik','Stamen.Watercolor'],
     overlays = ['OpenWeatherMap.Clouds'];
 
 var leaf_map = L.map('map', {
@@ -23,8 +23,88 @@ var leaf_map = L.map('map', {
 
 var layerControl = L.control.layers.provided(baseLayers, overlays).addTo(leaf_map);
 
-var C = require('./tax_map')
-},{"./tax_map":4,"leaflet":3,"leaflet-providers":2}],2:[function(require,module,exports){
+/**
+ * Created by kobi on 3/26/14.
+ */
+var geojson;
+
+var user_select = 4;
+
+var info;
+
+function getColor(d) {
+    return d > 500 ? '#BD0026' :
+        d > 200 ? '#E31A1C' :
+            d > 100 ? '#FC4E2A' :
+                d > 50 ? '#FD8D3C' :
+                    d > 20 ? '#FEB24C' :
+                        d > 10 ? '#FED976' :
+                            d > 1 ? '#800026' :
+                                '#FFEDA0';
+}
+
+function style(feature) {
+    return {
+        fillColor: getColor(feature.properties.id),
+        weight: 2,
+        opacity: 1,
+        color: 'white',
+        dashArray: '3',
+        fillOpacity: 0.7
+    };
+}
+
+function highlightFeature(e) {
+    var layer = e.target;
+
+    layer.setStyle({
+        weight: 5,
+        color: '#87AFC7',
+        dashArray: '',
+        fillOpacity: 0.99
+    });
+
+    if (!L.Browser.ie && !L.Browser.opera) {
+        layer.bringToFront();
+    }
+    info.update(layer.feature.properties);
+}
+function resetHighlight(e) {
+    geojson.resetStyle(e.target);
+    info.update();
+}
+
+function zoomToFeature(e) {
+    map.fitBounds(e.target.getBounds());
+}
+
+function filter_muni(feature,layer){
+    if (feature.properties.id == 1 ){
+        return false
+    }
+
+
+    return true;
+
+
+}
+function onEachFeature(feature, layer) {
+    if(feature.properties.id == 1){
+        return
+    }
+    layer.on({
+        mouseover: highlightFeature,
+        mouseout: resetHighlight,
+        click: zoomToFeature
+    });
+}
+
+geojson = L.geoJson(geo_data, {
+    style: style,
+    onEachFeature: onEachFeature,
+    filter: filter_muni
+}).addTo(leaf_map);
+},{"leaflet":3,"leaflet-providers":2}],2:[function(require,module,exports){
 (function () {
 	'use strict';
 
@@ -9675,86 +9755,4 @@ L.Map.include({
 
 
 }(window, document));
-},{}],4:[function(require,module,exports){
-/**
- * Created by kobi on 3/26/14.
- */
-var geojson;
-
-var user_select = 4;
-
-var info;
-
-function getColor(d) {
-    return d > 500 ? '#BD0026' :
-        d > 200 ? '#E31A1C' :
-            d > 100 ? '#FC4E2A' :
-                d > 50 ? '#FD8D3C' :
-                    d > 20 ? '#FEB24C' :
-                        d > 10 ? '#FED976' :
-                            d > 1 ? '#800026' :
-                                '#FFEDA0';
-}
-
-function style(feature) {
-    return {
-        fillColor: getColor(feature.properties.id),
-        weight: 2,
-        opacity: 1,
-        color: 'white',
-        dashArray: '3',
-        fillOpacity: 0.7
-    };
-}
-
-function highlightFeature(e) {
-    var layer = e.target;
-
-    layer.setStyle({
-        weight: 5,
-        color: '#87AFC7',
-        dashArray: '',
-        fillOpacity: 0.99
-    });
-
-    if (!L.Browser.ie && !L.Browser.opera) {
-        layer.bringToFront();
-    }
-    info.update(layer.feature.properties);
-}
-function resetHighlight(e) {
-    geojson.resetStyle(e.target);
-    info.update();
-}
-
-function zoomToFeature(e) {
-    map.fitBounds(e.target.getBounds());
-}
-
-function filter_muni(feature,layer){
-    if (feature.properties.id == 1 ){
-        return false
-    }
-
-
-    return true;
-
-
-}
-function onEachFeature(feature, layer) {
-    if(feature.properties.id == 1){
-        return
-    }
-    layer.on({
-        mouseover: highlightFeature,
-        mouseout: resetHighlight,
-        click: zoomToFeature
-    });
-}
-
-geojson = L.geoJson(geo_data, {
-    style: style,
-    onEachFeature: onEachFeature,
-    filter: filter_muni
-}).addTo(leaf_map);
 },{}]},{},[1])
